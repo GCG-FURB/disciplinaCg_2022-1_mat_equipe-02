9627f13aa8bea87e72af55febefaad77982d9ad7
Gabriel Reifegerste <=>
2022-04-25 16:38:22 -0300

    Code enhancement

--- unidade_2/CG-N2_1/Program.cs
+++ unidade_2/CG-N2_1/Program.cs
@@ -9,7 +9,7 @@ namespace CG_N2_1
     {
       List<Objeto> objetos = new List<Objeto>();
       char objetoId = Utilitario.charProximo('@');
-      ObjetoGeometria c1 = new Circulo(objetoId, null, new Ponto4D(0, 0, 0), 100, 72);
+      Circulo c1 = new Circulo(objetoId, null, new Ponto4D(0, 0, 0), 100, 72);
       c1.ObjetoCor.CorR = 255; c1.ObjetoCor.CorG = 255; c1.ObjetoCor.CorB = 0;
       objetos.Add(c1);
       Mundo window = Mundo.GetInstance(600, 600, objetos);
